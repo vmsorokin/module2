fd42a132ac872d51e6f600a1accedc0c27b71a8f
Vladimir <sorokin-v-m@yandex.ru>
2024-07-14 08:14:26 +0500

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,14 +1,27 @@
+# my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+# number = 0
+# while number < len(my_list):
+#     a = my_list[number]
+#     if a == 0:
+#         number = number + 1
+#         continue
+#     else:
+#         if a > 0:
+#             print(a)
+#             number = number + 1
+#             continue
+#         else:
+#             break
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 number = 0
 while number < len(my_list):
-    a = my_list[number]
-    if a == 0:
-        number = number + 1
-        continue
-    else:
+    a=my_list[number]
+    if a != 0:
         if a > 0:
             print(a)
             number = number + 1
             continue
         else:
             break
+    else: number = number + 1
+    continue
