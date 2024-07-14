099ffcff3f2a1ab339ff84daa2fd54a115c577a8
Vladimir <sorokin-v-m@yandex.ru>
2024-07-15 01:39:42 +0500

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,20 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8,9,10,11,12,13,14,15]
+primes = []
+not_primes = []
+is_prime = True
+# # primes.append(numbers[2]) - добавление в список
+for i in numbers[1:len(numbers)]:
+    # print(i)
+    for j in range(2, i):
+        # print(j)
+        a=i%j
+        # print(f'{i}%{j}={i%j}')
+        if a == 0:
+            is_prime = False
+            break
+        else: is_prime = True
+    if is_prime == True:
+        primes.append(i)
+    else: not_primes.append(i)
+print('Простые', primes)
+print('Составные', not_primes)
